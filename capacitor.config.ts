12540f309b35cfc70ca72e610862b8afb830cfe6
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-08 17:33:07 +0700

    My first VueJS ft Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.shopionic',
+  appName: 'shopionic',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
