30eace0b55bfd36c43e640081206ae5b4c1569f8
ngogiaphat <ngogiaphat0802@gmail.com>
2023-04-08 18:00:42 +0700

    converted project to JS

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,6 @@
   </ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+<script>
+  import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
